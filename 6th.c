94d14aa8620af7d1e32d99bcd5b50bf07aed53eb
Saksham <sakshambhat76@gmail.com>
2026-09-16 22:31:36 +0530

    product of two no.

---
+++ 6th.c
@@ -0,0 +1,13 @@
+/* Product of two no. */
+# include <stdio.h>
+int main(){
+    int a, b, product;
+
+    printf("aenter two no. : ");
+    scanf("%d %d",&a,&b);
+
+    product = a*b;
+
+    printf("product = %d", product);
+    return 0;
+}
