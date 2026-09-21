99ce5fd5a6362df8d764b6270dd08db0159f878a
Saksham <sakshambhat76@gmail.com>
2026-09-22 00:04:04 +0530

    division of a no.

---
+++ 7th.c
@@ -0,0 +1,10 @@
+// divide two no
+# include <stdio.h>
+int main(){
+    float a,b,result;
+    printf("enter two no : ");
+    scanf("%f %f",&a,&b);
+    result = a/b;
+    printf("Division = %.2f",result);
+    return 0;
+}
